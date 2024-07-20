16101ce7a52d02b4793ee35b4d1d854216a046d1
Ceyda <ceydaonemli2@gmail.com>
2024-07-20 13:12:16 +0300

    feat: product service file created

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { productService } from "../../service/product/product.service.ts";
+import styles from "./Product.module.scss";
 
 interface Product {
   id: number;
@@ -20,108 +22,50 @@ interface RootState {
 }
 
 const Product = () => {
-  const products = useSelector(
+  const productsFromState = useSelector(
     (state: RootState) => state.allProducts.products
   );
 
-  if (products.length === 0) {
-    return <div>No products available</div>;
-  }
+  const [products, setProducts] = useState<Product[]>(productsFromState);
+
+  const getProducts = async () => {
+    try {
+      const response = await productService.getAllProducts();
+      if (response && response.data) {
+        setProducts(response.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
 
   return (
-    <div className="productGrid">
-      {products.map((product) => (
-        <div key={product.id} className="cardContainer">
-          <div className="imageContainer">
-            <img
-              src={product.image}
-              alt={product.name}
-              className="productImage"
-            />
+    <div className={styles.productGrid}>
+      {products.length === 0 ? (
+        <div>No products available</div>
+      ) : (
+        products.map((product) => (
+          <div key={product.id} className={styles.cardContainer}>
+            <div className={styles.imageContainer}>
+              <img
+                src={product.image}
+                alt={product.name}
+                className={styles.productImage}
+              />
+            </div>
+            <div className={styles.price}>{product.price} ₺</div>
+            <div className={styles.productName}>{product.name}</div>
+            <div className={styles.productDescription}>
+              {product.description}
+            </div>
+            <button className={styles.addToCartButton}>Add to Cart</button>
           </div>
-          <div className="price">{product.price} ₺</div>
-          <div className="productName">{product.name}</div>
-          <div className="productDescription">{product.description}</div>
-          <button className="addToCartButton">Add to Cart</button>
-        </div>
-      ))}
-      <style jsx>{`
-        .productGrid {
-          display: flex;
-          flex-wrap: wrap;
-          justify-content: space-around;
-          gap: 16px;
-        }
-
-        .cardContainer {
-          width: 200px;
-          height: 250px;
-          display: flex;
-          flex-direction: column;
-          justify-content: space-between;
-          align-items: center;
-          border: 1px solid #ddd;
-          border-radius: 8px;
-          padding: 8px;
-          margin: 16px;
-          box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-        }
-
-        .imageContainer {
-          width: 100%;
-          height: 130px;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          background-color: #f0f0f0;
-          margin-bottom: 8px;
-        }
-
-        .productImage {
-          width: 100%;
-          height: 100%;
-          object-fit: cover;
-        }
-
-        .price {
-          color: #007bff;
-          font-weight: bold;
-          margin-bottom: 4px;
-        }
-
-        .productName {
-          font-weight: bold;
-          margin-bottom: 4px;
-          text-align: center;
-        }
-
-        .productDescription {
-          color: #555;
-          font-size: 12px;
-          margin-bottom: 8px;
-          text-align: center;
-          overflow: hidden;
-          text-overflow: ellipsis;
-          display: -webkit-box;
-          -webkit-line-clamp: 3; /* 3 satır */
-          -webkit-box-orient: vertical;
-        }
-
-        .addToCartButton {
-          width: 100%;
-          padding: 8px;
-          background-color: #007bff;
-          color: white;
-          border: none;
-          border-radius: 4px;
-          cursor: pointer;
-          font-size: 14px;
-        }
-
-        .addToCartButton:hover {
-          background-color: #0056b3;
-        }
-      `}</style>
+        ))
+      )}
     </div>
   );
 };
---
+++ src/service/product/product.service.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const API_URL = "https://5fc9346b2af77700165ae514.mockapi.io/products";
+
+const getAllProducts = async () => {
+  const response = await axios.get(API_URL).catch((err) => {
+    console.log(err);
+  });
+
+  return response;
+};
+
+export const productService = {
+  getAllProducts,
+};
